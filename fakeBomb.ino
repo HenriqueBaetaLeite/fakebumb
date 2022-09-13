41ee34a4e3426cf1a887fcef43a42440f021c134
HenriqueBaetaLeite <hbaetaleite@gmail.com>
2022-09-13 08:45:43 -0300

    feat: BlinkLeds fixed, creating variables for ledState

--- fakeBomb.ino
+++ fakeBomb.ino
@@ -10,6 +10,8 @@ bool redLedState = false;
 bool greenLedState = false;
 bool yellowLedState = false;
 
+bool allLedsState = false;
+
 bool gameNotStarted = true;
 
 const int disarmBombButton = 5;
@@ -29,9 +31,13 @@ unsigned long lastTime3 = 0;
 
 unsigned long lastTimeBlinkLeds = 0;
 
+unsigned long twoSecondsInterval = 2000;
+
+// Fix time here:
 unsigned long twoMinutesInterval = 2000;
 unsigned long fourMinutesInterval = 4000;
 unsigned long sixMinutesInterval = 6000;
+
 
 unsigned long totalTime = 10000;
 
@@ -39,19 +45,16 @@ bool itIsOpen = false;
 
 void (*resetFunc)(void) = 0;
 
-void blinkLeds() {
+void blinkLeds()
+{
   unsigned long actualTime = millis();
-  if (actualTime - lastTimeBlinkLeds >= 1500) {
-    digitalWrite(ledGreenBomb, HIGH);
-    digitalWrite(ledRedBomb, HIGH);
-    digitalWrite(ledYellowBomb, HIGH);
+  if (actualTime - lastTimeBlinkLeds >= twoSecondsInterval)
+  {
     lastTimeBlinkLeds = millis();
-  }
-  else {
-    digitalWrite(ledGreenBomb, LOW);
-    digitalWrite(ledRedBomb, LOW);
-    digitalWrite(ledYellowBomb, LOW);
-    // lastTimeBlinkLeds = millis();
+    allLedsState = !allLedsState;
+    digitalWrite(ledGreenBomb, allLedsState);
+    digitalWrite(ledRedBomb, allLedsState);
+    digitalWrite(ledYellowBomb, allLedsState);
   }
 }
 
@@ -60,21 +63,18 @@ void ledTimerSync()
   unsigned long actualTime = millis();
   if (actualTime - lastTime >= twoMinutesInterval)
   {
-    // greenLedState = !greenLedState;
     digitalWrite(ledGreenBomb, greenLedState);
     lastTime = actualTime;
   }
 
   if (actualTime - lastTime2 >= fourMinutesInterval)
   {
-    // yellowLedState = !yellowLedState;
     digitalWrite(ledYellowBomb, yellowLedState);
     lastTime2 = actualTime;
   }
 
   if (actualTime - lastTime3 >= sixMinutesInterval)
   {
-    // redLedState = !redLedState;
     digitalWrite(ledRedBomb, redLedState);
     lastTime3 = actualTime;
   }
@@ -91,20 +91,18 @@ void setup()
   pinMode(ledRedBomb, OUTPUT);
   pinMode(ledYellowBomb, OUTPUT);
   pinMode(ledGreenBomb, OUTPUT);
-  digitalWrite(ledRedBomb, HIGH);
-  digitalWrite(ledYellowBomb, HIGH);
-  digitalWrite(ledGreenBomb, HIGH);
-
   pinMode(ledInside, OUTPUT);
 
   servo.attach(myServo);
 
-   servo.slowmove(100, 20);
+  servo.slowmove(100, 20);
 
-  while(gameNotStarted) {
-    Serial.println("Será q deu certo???");
+  while (gameNotStarted)
+  {
     blinkLeds();
-    if(digitalRead(startOpenBombButton) == LOW) {
+
+    if (digitalRead(startOpenBombButton) == LOW)
+    {
       Serial.prinln("Button pressed...");
       gameNotStarted = false;
     }
